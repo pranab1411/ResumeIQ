03cb7597f26cb63430ebcbc2d4be6c5ac5223a03
Pranab Chourasiya <pranab.chourasiya@gmail.com>
2026-08-27 17:07:40 +0530

    chore: save updated version config metadata

--- config/smtp_config.py
+++ config/smtp_config.py
@@ -5,7 +5,7 @@ Loads credentials securely from environment variables.
 import os
 
 # Admin Sender Email Address (Configured via Environment Variable)
-DEFAULT_SMTP_USER = os.getenv("RESUMEIQ_SMTP_USER", "pranabchourasiya876@gmail.com")
+DEFAULT_SMTP_USER = os.getenv("RESUMEIQ_SMTP_USER", "support.resumeiq@gmail.com")
 
 # Admin Google App Password (Configured via Environment Variable)
 DEFAULT_SMTP_PASSWORD = os.getenv("RESUMEIQ_SMTP_PASSWORD", "")
